a3a434f85422333dcdac090ad7b27a5a19f705b2
karkran <nikhil.karkra@anz.com>
2018-04-29 20:03:22 +1000

    setting angular google maps

--- src/pages/add-place/add-place.ts
+++ src/pages/add-place/add-place.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, ModalController } from 'ionic-angular';
 import {NgForm} from '@angular/forms'
+import {SetLocationPage} from "../set-location/set-location";
 @IonicPage()
 @Component({
   selector: 'page-add-place',
   templateUrl: 'add-place.html',
 })
 export class AddPlacePage {
+  constructor(private modalCtrl: ModalController) {}
   onSubmit(form:NgForm){
     console.log(form.value)
   }
-  // constructor(public navCtrl: NavController, public navParams: NavParams) {
-  // }
   //
   // ionViewDidLoad() {
   //   console.log('ionViewDidLoad AddPlacePage');
   // }
+  onOpenMap(){
+    const modal = this.modalCtrl.create(SetLocationPage)
+    modal.present();
+  }
 
 }
